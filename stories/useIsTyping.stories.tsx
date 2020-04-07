be2e04c9ee6bef9f6ca1da350e4a63dca2afc1b1
KATT <alexander@n1s.se>
2020-04-07 12:23:25 +0200

    🎨 Lint `stories/`

--- stories/useIsTyping.stories.tsx
+++ stories/useIsTyping.stories.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useIsTyping } from '../src';
-import {TextField, Box, Typography} from '@material-ui/core';
-
+import { TextField, Box, Typography } from '@material-ui/core';
 
 export default {
   title: 'Default',
@@ -12,20 +11,20 @@ export const Input = () => {
   return (
     <div>
       <input ref={register} placeholder="type something here" />
-      <br/>
+      <br />
       Typing? {isTyping ? '✅' : '❌'}
     </div>
-  )
+  );
 };
 export const TextArea = () => {
   const [isTyping, register] = useIsTyping();
   return (
     <div>
       <textarea ref={register} placeholder="type something here" />
-      <br/>
+      <br />
       Typing? {isTyping ? '✅' : '❌'}
     </div>
-  )
+  );
 };
 
 export const MaterialUITextField = () => {
@@ -34,54 +33,58 @@ export const MaterialUITextField = () => {
     <div>
       <TextField label="Type something here" inputRef={register} />
       <Box mb={2} />
-      <Typography variant="body1">
-        Typing? {isTyping ? '✅' : '❌'}
-      </Typography>
+      <Typography variant="body1">Typing? {isTyping ? '✅' : '❌'}</Typography>
     </div>
-  )
+  );
 };
 
 export const TriggerEvents = () => {
   const [isTyping, register] = useIsTyping();
   useEffect(() => {
-    console.log('typing?', isTyping)
+    console.log('typing?', isTyping);
 
     // [...] call API
   }, [isTyping]);
 
   useEffect(() => {
     return () => {
-      console.log('unmounted -- probably stopped typing')
+      console.log('unmounted -- probably stopped typing');
 
       // [..] call API
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <div>
       Look into console log when using this
-      <br/>
+      <br />
       <input ref={register} placeholder="type something here" />
-      <br/>
+      <br />
       Typing? {isTyping ? '✅' : '❌'}
     </div>
-  )
+  );
 };
 
 export const Default = () => {
   const [isTyping, register] = useIsTyping();
-  const [isMounted, setIsMounted] = useState(true)
+  const [isMounted, setIsMounted] = useState(true);
   return (
     <div>
       <label>
-        <input type="checkbox" checked={isMounted} onChange={(e) => setIsMounted(e.target.checked)} />
+        <input
+          type="checkbox"
+          checked={isMounted}
+          onChange={e => setIsMounted(e.target.checked)}
+        />
         Is mounted?
       </label>
       <div>
-        {isMounted && <input ref={register} placeholder="type something here" />}
-        <br/>
+        {isMounted && (
+          <input ref={register} placeholder="type something here" />
+        )}
+        <br />
         Typing? {isTyping ? '✅' : '❌'}
       </div>
     </div>
-  )
+  );
 };
